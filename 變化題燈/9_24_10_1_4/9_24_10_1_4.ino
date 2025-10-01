3fc2d276c585c9eb1a9d9332e5f09831b5235c8f
yukauchi0v0 <147982451+yukauchi0v0@users.noreply.github.com>
2025-10-01 15:47:50 +0800

    10/1-4

---
+++ 變化題燈/9_24_10_1_4/9_24_10_1_4.ino
@@ -0,0 +1,110 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://docs.arduino.cc/hardware/
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  https://docs.arduino.cc/built-in-examples/basics/Blink/
+*/
+
+// the setup function runs once when you press reset or power the board
+const int ButtonPin = 2;
+const int RledPin = 3;
+const int GledPin = 4;
+const int BledPin = 5;
+
+int buttonState = 0;
+int ledcolor = 0;
+
+bool ButtonPressed = false;
+
+
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(RledPin, OUTPUT);
+  pinMode(GledPin, OUTPUT);
+  pinMode(BledPin, OUTPUT);
+
+  pinMode(ButtonPin, INPUT);
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  //Green
+  buttonState = digitalRead(ButtonPin);
+
+  
+if(buttonState == HIGH && !ButtonPressed ){
+  ledcolor = ledcolor + 1;
+  ButtonPressed = true;
+  }
+  if(buttonState == LOW && !ButtonPressed){
+    ButtonPressed = false;
+  }
+  if(ledcolor == 0){
+    digitalWrite(RledPin, HIGH);
+    digitalWrite(GledPin, HIGH);
+    digitalWrite(BledPin, HIGH);
+  } 
+  else if(ledcolor == 1){
+    digitalWrite(RledPin, LOW);
+    digitalWrite(GledPin, HIGH);
+    digitalWrite(BledPin, HIGH);
+  }
+  else if(ledcolor == 2){
+    digitalWrite(RledPin, HIGH);
+    digitalWrite(GledPin, LOW);
+    digitalWrite(BledPin, HIGH);
+  }
+  else if(ledcolor == 3){
+    digitalWrite(RledPin, HIGH);
+    digitalWrite(GledPin, HIGH);
+    digitalWrite(BledPin, LOW);
+  }
+   else if(ledcolor == 4){
+    digitalWrite(RledPin, LOW);
+    digitalWrite(GledPin, LOW);
+    digitalWrite(BledPin, HIGH);
+  }
+  else if(ledcolor == 5){
+    digitalWrite(RledPin, LOW);
+    digitalWrite(GledPin, HIGH);
+    digitalWrite(BledPin, LOW);
+  }
+  else if(ledcolor == 6){
+    digitalWrite(RledPin, HIGH);
+    digitalWrite(GledPin, LOW);
+    digitalWrite(BledPin, LOW);
+  }
+  else if(ledcolor == 7){
+    digitalWrite(RledPin, LOW);
+    digitalWrite(GledPin, LOW);
+    digitalWrite(BledPin, LOW);
+  }
+   else if(ledcolor == 8){
+   ledcolor = 0;
+  }
+}
+
+
+
+   
+       
+
